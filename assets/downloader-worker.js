6ab18b11c7b7a2d618e9109a83f588e1e1d149d2
lucas <lucasa@gmail.com>
2018-10-24 01:29:10 -0300

    Updates

--- assets/downloader-worker.js
+++ assets/downloader-worker.js
@@ -1,14 +1,3 @@
-'use strict';
+/*! wordcloud-app  2018-10-21 */
 
-self.callback = function() {
-  var argsArray = Array.prototype.slice.call(arguments);
-  self.postMessage(argsArray);
-};
-
-self.onmessage = function(evt) {
-  var url = evt.data;
-  url += (url.indexOf('?') === -1) ? '?' : '&';
-  url += 'callback=callback';
-
-  self.importScripts(url);
-};
+"use strict";self.callback=function(){var a=Array.prototype.slice.call(arguments);self.postMessage(a)},self.onmessage=function(a){var b=a.data;b+=-1===b.indexOf("?")?"?":"&",b+="callback=callback",self.importScripts(b)};
--- assets/vars-sample.js
+++
@@ -1,5 +0,0 @@
-var FACEBOOK_APP_ID = '';
-var GOOGLE_CLIENT_ID = '';
-var IMGUR_CLIENT_ID = '';
-
-var DONATE_HTML = '';
